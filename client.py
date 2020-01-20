75866fe4ab4b7c67aeb070abd2944d9993a66226
nasopida <nasopida@naver.com>
2020-01-20 14:05:30 +0900

    update client

--- client.py
+++ client.py
@@ -1,10 +1,13 @@
 import socket
 import argparse
 import threading
-#접속하고 싶은 ip와 port를 입력받는 클라이언트 코드를 작성해보자.
-# 접속하고 싶은 포트를 입력한다.
+import random
+from datetime import datetime
 port = 57270
 host = "127.0.0.1"
+
+def dice():
+    print(random.randint(1,6))
 
 def handle_receive(lient_socket, user):
     while 1:
@@ -18,22 +21,26 @@ def handle_receive(lient_socket, user):
             print(data)
 
 def handle_send(client_socket):
+    now=datetime.now()
     while 1:
-        data = input(user +' : ')
+        print(user+": ",end='')
+        data=input()
         client_socket.send(data.encode('utf-8'))
         if data == "/quit":
             break
+        if data=="/whoami":
+            print(user+"입니다")
+        if data=="/whattime":
+            print("%s시 %s분 %s초입니다."%(now.hour,now.minute,now.second))
+        if data=="/whatdate":
+            print("%s년 %s월 %s일입니다."%(now.year,now.month,now.day))
+        if data=="/dice":
+            dice()
     client_socket.close()
 
 
 if __name__ == '__main__':
-    #parser와 관련된 메서드 정리된 블로그 : https://docs.python.org/ko/3/library/argparse.html
-    #description - 인자 도움말 전에 표시할 텍스트 (기본값: none)
-    #help - 인자가 하는 일에 대한 간단한 설명.
-    #nargs - 소비되어야 하는 명령행 인자의 수. -> '+'로 설정 시 모든 명령행 인자를 리스트로 모음 + 없으면 경고
-    #required - 명령행 옵션을 생략 할 수 있는지 아닌지 (선택적일 때만).
-    #parser = argparse.ArgumentParser(description="\nJoo's client\n-p port\n-i host\n-s string")
-    parser = argparse.ArgumentParser(description="\nJoo's client\n-p port\n-i host\n-s string")
+    parser = argparse.ArgumentParser(description="\nclient\n-p port\n-i host\n-s string")
     parser.add_argument('-p', help="port")
     parser.add_argument('-i', help="host")
     parser.add_argument('-u', help="user", required=True)
@@ -65,32 +72,3 @@ if __name__ == '__main__':
 
     send_thread.join()
     receive_thread.join()
-
-"""
-# 소켓 라이브러리 포함
-import socket
-
-HOST = '127.0.0.1'
-PORT = 57270
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# connect에 들어가는건 튜플이기 떄문에 괄호가 두개임
-client_socket.connect((HOST,PORT))
-
-#키보드로 입력한 문자열을 서버로 전송
-#서버에서 에코되어 들어오는 메세지를 받으면 화면에 출력
-#quit를 입력할 때 까지 반복
-while True:
-    message = input('Enter Message : ')
-    if message == 'quit':
-        break
-    
-    # 메세지를 encode해서 보낸다.
-    client_socket.send(message.encode())
-    data = client_socket.recv(1024)
-
-    print('Received from the server : ',repr(data.decode()))
-
-client_socket.close()
-"""
