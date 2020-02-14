8918bdcb7d6af1e1f5c78e70f5e9c5808194aea3
현석 송 <jhn06046@hotmail.com>
2020-02-14 09:29:18 +0900

    일단 푸쉬

--- client/userList.py
+++ client/userList.py
@@ -2,6 +2,7 @@ from tkinter import *
 import guiClient
 import ctypes
 import tkinter as tk
+import tkinter.ttk
 
 import os
 import sys
@@ -26,19 +27,54 @@ class UserList:
 
         #유저수 출력 라벨
         self.printUserFrame = Frame(self.mainFrame)
-        self.printUserFrame.pack(expand = True, pady = 5)
+        self.printUserFrame.pack(side=TOP, expand = True, pady = 5)
         self.printUserLabel = Label(self.printUserFrame, text="ㅎㅇ")
-        self.printUserLabel.pack(expand = True)
+        self.printUserLabel.pack(side=TOP, expand=True)
 
         #유저를 담는 라벨
-        self.userFrame = Frame(self.mainFrame, background="yellow")
-        self.userFrame.pack(fill=X, side=TOP)
+        
+        self.userFrame = tk.Frame(self.mainFrame, width=300, height=540, relief='raised',borderwidth=1)
+        self.userFrame.pack(fill=BOTH, expand=True)
 
-        #self.addUser('안녕')
-        # 삭제 버튼
-        self.deleteBtn =  Button(self.mainFrame, command=self.deleteUser, text="삭제")
-        self.deleteBtn.pack(side=BOTTOM)
+        self.userScrollbar = Scrollbar(self.userFrame)
+        self.userScrollbar.pack(side=RIGHT, fill=Y)
 
+
+        #버튼들의 프레임
+        self.btnFrame = tk.Frame(self.mainFrame)
+        self.btnFrame.pack(fill=X, side=BOTTOM)
+        # 강퇴 요청 버튼
+        self.deleteBtn =  Button(self.btnFrame,width=20, command=self.deleteUser, text="강퇴 요청")
+        self.deleteBtn.pack(side=LEFT)
+        # 차단 버튼
+        self.blackUserBtn = Button(self.btnFrame, width=20, command=self.blackUser, text="차단")
+        self.blackUserBtn.pack(side=RIGHT)
+
+        
+    
+    # 아래 2개 함수는 유저가 들어올때 / 나갈때 자동 호출되도록 구현
+    # 유저를 추가시키는 함수
+    def addUser(self, userName):
+        global rowLevel
+        tempVar = BooleanVar(value=False)
+        tempFrame = Frame(self.userFrame)
+        temp = Checkbutton(tempFrame, variable=tempVar, text = userName)
+        self.checkBoxList.append(tempVar)
+        tempFrame.pack(side=TOP, fill=X)
+        temp.pack(side=LEFT)
+        self.userCnt += 1
+        self.printUserLabel["text"] = "user : " + str(self.userCnt)
+        
+
+    # 유저를 제거하는 함수
+    def deleteUser(self, userName):
+        pass
+
+    # 유저를 차단하는 함수, 구현순위 맨 뒤
+    def blackUser(self, userName):
+        pass
+
+    #가운데로 오게 하는 함수
     def centerWindow(self, window):
         width = 300
         height = 600
@@ -49,24 +85,3 @@ class UserList:
         x = screen_width / 2 + 200 + 3
         y = screen_height/2 - height/2
         window.geometry('%dx%d+%d+%d' %(width,height,x,y))
-
-    
-    # 아래 2개 함수는 유저가 들어올때 / 나갈때 자동 호출되도록 구현
-    # 유저를 추가시키는 함수
-    def addUser(self, userName):
-        global rowLevel
-        self.userCnt += 1
-        self.printUserLabel["text"] = "user : " + str(self.userCnt)
-        tempVar = tk.IntVar()
-        tempCheck = tk.Checkbutton(self.userFrame,text=userName,variable=tempVar)
-        tempCheck.grid(column=0, row=rowLevel)
-        rowLevel += 1
-
-    # 유저를 제거하는 함수
-    def deleteUser(self, userName):
-        pass
-
-    # 유저를 차단하는 함수, 구현순위 맨 뒤
-    def blackUser(self, userName):
-        pass
-
