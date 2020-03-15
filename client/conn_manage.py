a8dcc82b9e1877203a9c5924cfb7516fce6d8e41
Keinahas <kaidrielle@gmail.com>
2020-03-15 11:47:42 +0900

    Update conn_manage.py

--- client/conn_manage.py
+++ client/conn_manage.py
@@ -28,7 +28,7 @@ class conn_manage:
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect(self):
-        DEBUG("connecting on {} port {}".format(address[0], address[1]))
+        self.DEBUG("connecting on {} port {}".format(self.address[0], self.address[1]))
         try:
             self.client_socket.connect(self.address)
         except Exception:
@@ -64,9 +64,13 @@ class conn_manage:
         self.create()
         self.connect()
         if self.connection == None:
-            self.connection = Process(target=self.__start, args=(self.address,))
-            self.connection.start()
-            self.DEBUG("connection to server successfully established...")
+            self.connection = Process(target=self.__start, args=(self.address, 60))
+            try:
+                self.connection.start()
+                self.DEBUG("connection to server successfully established...")
+            except Exception:
+                
+                self.DEBUG("connection not established...")
 
     def stop(self):
         if self.connection != None:
@@ -81,18 +85,18 @@ class conn_manage:
 
         while writeSockList:
             # Wait for at least one of the sockets to be ready for processing
-            DEBUG("\nwaiting for the next event")
+            self.DEBUG("\nwaiting for the next event")
             readable, writable, exceptional = select.select(readSockList, writeSockList, readSockList, timeout)
 
             # Handle "exceptional conditions"
             for s in exceptional:
-                DEBUG("handling exceptional condition for" + s.getpeername())
+                self.DEBUG("handling exceptional condition for" + s.getpeername())
                 # Stop listening for input on the connection
                 if not self.reconnect:
                     self.disconnect()
 
             # Handle outputs
-            DEBUG("\nHandle outputs------")
+            self.DEBUG("\nHandle outputs------")
             for s in writable:
                 try:
                     next_msg = self.output.get_nowait()
@@ -101,28 +105,28 @@ class conn_manage:
                     # DEBUG("output queue for "+Clients[s]+" is empty")
                     continue
                 else:
-                    DEBUG("writing message to server")
-                    DEBUG("message :" + str(next_msg))
+                    self.DEBUG("writing message to server")
+                    self.DEBUG("message :" + str(next_msg))
                     s.send(next_msg.encode())
 
             # Handle inputs
-            DEBUG("\nHandle inputs------")
+            self.DEBUG("\nHandle inputs------")
             for s in readable:
                 try:
                     data = s.recv(1024)
                 # 이곳에 유저가 나간 것을 처리
                 except ConnectionResetError:
-                    DEBUG("ConnectionResetError!!")
+                    self.DEBUG("ConnectionResetError!!")
                     break
 
                 if data:
                     # A readable client socket has data
-                    DEBUG("{} : {}".format(str(s.getpeername()), data.decode('utf-8')))
+                    self.DEBUG("{} : {}".format(str(s.getpeername()), data.decode('utf-8')))
 
                     try:
                         parsed = packet.toPacket(data.decode('utf-8'))
                     except Exception as err:
-                        DEBUG("Exception: {}".format(err))
+                        self.DEBUG("Exception: {}".format(err))
                         print("NOT PACKET!!! -- client err")
                         continue
 
